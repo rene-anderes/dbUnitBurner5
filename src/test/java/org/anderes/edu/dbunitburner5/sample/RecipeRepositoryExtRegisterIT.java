176ba8d9fa83935753100aed9382f4e1fa840a42
René Anderes <randeres@yahoo.de>
2019-12-24 14:36:45 +0100

    Nun auch mittes @ExtensionRegister verwendbar

---
+++ src/test/java/org/anderes/edu/dbunitburner5/sample/RecipeRepositoryExtRegisterIT.java
@@ -0,0 +1,161 @@
+package org.anderes.edu.dbunitburner5.sample;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertThat;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.anderes.edu.dbunitburner5.CleanupUsingScript;
+import org.anderes.edu.dbunitburner5.DbUnitExtension;
+import org.anderes.edu.dbunitburner5.ShouldMatchDataSet;
+import org.anderes.edu.dbunitburner5.UsingDataSet;
+import org.anderes.edu.dbunitburner5.UsingDataSetScript;
+import org.anderes.edu.dbunitburner5.sample.data.Ingredient;
+import org.anderes.edu.dbunitburner5.sample.data.Recipe;
+import org.anderes.edu.dbunitburner5.sample.data.RecipeRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ExtendWith(DbUnitExtension.class)
+@ContextConfiguration(locations = { "classpath:/sample/application-context.xml" })
+public class RecipeRepositoryExtRegisterIT {
+
+    @Inject
+    private RecipeRepository repository;
+    @Inject @SuppressWarnings("unused")
+    private DataSource datasource; /* Required for extension */
+        
+    @Test
+    @UsingDataSet(value = { "/sample/prepare.json" })
+    @ShouldMatchDataSet(
+            value = { "/sample/prepare.json" },
+            orderBy = { "RECIPE.UUID", "INGREDIENT.ID" })
+    public void shouldBeFindAll() {
+        Iterable<Recipe> recipes = repository.findAll();
+        assertThat(recipes, is(notNullValue()));
+        assertThat(recipes.iterator().hasNext(), is(true));
+        int counter = 0;
+        for (Recipe recipe : recipes) {
+            assertThat(recipe.getTitle(), is(notNullValue()));
+            counter++;
+        }
+        assertThat(counter, is(2));
+    }
+    
+    @Test
+    @UsingDataSetScript(value = { "/sql/LoadTestdata.sql" })
+    public void shouldBeFindAllTestdata() {
+        Iterable<Recipe> recipes = repository.findAll();
+        assertThat(recipes, is(notNullValue()));
+        assertThat(recipes.iterator().hasNext(), is(true));
+        int counter = 0;
+        for (Recipe recipe : recipes) {
+            assertThat(recipe.getTitle(), is(notNullValue()));
+            counter++;
+        }
+        assertThat(counter, is(5));
+    }
+    
+    @Test
+    @UsingDataSet(value = { "/sample/prepare.xls" })
+    public void shouldBeOneRecipe() {
+        final Optional<Recipe> recipe = repository.findById("c0e5582e-252f-4e94-8a49-e12b4b047afb");
+        assertThat(recipe.isPresent(), is(true));
+        assertThat(recipe.get().getTitle(), is("Arabische Spaghetti"));
+    }
+    
+    @Test
+    @UsingDataSet(value = { "/sample/prepare.xls" })
+    @ShouldMatchDataSet(
+            value = { "/sample/prepare.xls" },
+            orderBy = { "RECIPE.UUID", "INGREDIENT.ID" })
+    public void getRecipesByTitle() {
+        final Collection<Recipe> recipes = repository.findByTitleLike("%Spaghetti%");
+
+        assertNotNull(recipes);
+        assertThat(recipes.size(), is(1));
+        final Recipe recipe = recipes.iterator().next();
+        assertThat(recipe.getTitle(), is("Arabische Spaghetti"));
+    }
+    
+    @Test
+    @CleanupUsingScript(value = { "/sql/DeleteTableContentScript.sql" })
+    @UsingDataSet(value = { "/sample/prepare.xls" })
+    public void shouldBeSaveNewRecipe() {
+        // given
+        final Recipe newRecipe = RecipeBuilder.buildRecipe();
+        
+        // when
+        final Recipe savedRecipe = repository.save(newRecipe);
+        
+        // then
+        assertThat(savedRecipe, is(not(nullValue())));
+        assertThat(savedRecipe.getUuid(), is(not(nullValue())));
+        
+        final Optional<Recipe> findRecipe = repository.findById(savedRecipe.getUuid());
+        assertThat(findRecipe.isPresent(), is(true));
+        assertNotSame(newRecipe, findRecipe.get());
+        assertThat(newRecipe, is(findRecipe.get()));
+    }
+    
+    @Test
+    @UsingDataSet(value = { "/sample/prepare.xls" })
+    @ShouldMatchDataSet(value = { "/sample/expected-afterUpdate.xls" },
+            excludeColumns = { "INGREDIENT.ID" },
+            orderBy = { "RECIPE.UUID", "INGREDIENT.ANNOTATION" }
+    )
+    public void shouldBeUpdateRecipe() {
+        final Optional<Recipe> updateRecipe = repository.findById("c0e5582e-252f-4e94-8a49-e12b4b047afb");
+        assertThat(updateRecipe.isPresent(), is(true));
+        updateRecipe.get().setPreamble("Neuer Preamble vom Test");
+        updateRecipe.get().addIngredient(new Ingredient("1", "Tomate", "vollreif"));
+        final Recipe savedRecipe = repository.save(updateRecipe.get());
+        
+        assertThat(savedRecipe, is(not(nullValue())));
+        assertThat(savedRecipe.getPreamble(), is("Neuer Preamble vom Test"));
+        assertThat(savedRecipe.getIngredients().size(), is(4));
+        
+        final Optional<Recipe> findRecipe = repository.findById(savedRecipe.getUuid());
+        assertThat(findRecipe.isPresent(), is(true));
+        final Recipe recipe = findRecipe.get();
+        assertThat(recipe.getPreamble(), is("Neuer Preamble vom Test"));
+        assertNotSame(updateRecipe.get(), recipe);
+        assertThat(updateRecipe.get(), is(recipe));
+    }
+    
+    @Test
+    @UsingDataSet(value = { "/sample/prepare.xls" })
+    @ShouldMatchDataSet(value = { "/sample/expected-afterDeleteOne.xls" },
+            excludeColumns = { "RECIPE.ADDINGDATE" },
+            orderBy = { "RECIPE.UUID", "INGREDIENT.ID" })
+    public void shouldBeDelete() {
+        final Optional<Recipe> toDelete = repository.findById("c0e5582e-252f-4e94-8a49-e12b4b047afb");
+        assertThat("Das Rezept mit der ID 'c0e5582e-252f-4e94-8a49-e12b4b047afb' existiert nicht in der Datenbank", toDelete.isPresent(), is(true));
+        repository.delete(toDelete.get());
+        
+        final Collection<Recipe> recipes = repository.findByTitleLike("%Spaghetti%");
+        assertNotNull(recipes);
+        assertThat(recipes.size(), is(0));
+    }
+    
+    @Test
+    @UsingDataSet(value = { "/sample/prepare.xls" })
+    public void shouldBeFindAllTag() {
+        final List<String> tags = repository.findAllTag();
+        assertThat(tags, is(notNullValue()));
+        assertThat(tags.size(), is(4));
+    }
+}
